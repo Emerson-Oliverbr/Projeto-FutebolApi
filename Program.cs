3b0dbabc370ba779b8204136ba7df8149b5c02a3
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-09-12 23:27:38 -0300

    Configuração do Entity Framework + Migração do Banco de Dados

--- Program.cs
+++ Program.cs
@@ -1,36 +1,13 @@
+using FutebolApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(Options => Options.UseSqlite("Data Source=Futebol.db"));
+
 var app = builder.Build();
 
-app.MapGet("/usuarios", () =>
-{
-    List<string> nomes = new List<string>
-    {
-        "João",
-        "Maria",
-        "Luizinho",
-    };
-
-    return nomes;
-});
-
-app.MapPost("/usuarios", (string nome) =>
-{
-    return Results.Ok($"Usuário {nome} criado com sucesso!");
-});
-
-app.MapPut("/usuarios/{id}", (int id, string novoNome) =>
-{
-    return Results.Ok($"Usuário {id} atualizado para {novoNome}");
-});
-
-app.MapPatch("/usuarios/{id}", (int id, string campo, string valor) =>
-{
-    return Results.Ok($"Usuário {id} teve o campo {campo} atualizado para {valor}");
-});
-
-app.MapDelete("/usuarios/{id}", (int id) =>
-{
-    return Results.Ok($"Usuário {id} removido com sucesso!");
-});
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
