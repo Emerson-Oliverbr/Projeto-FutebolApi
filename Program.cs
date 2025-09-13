32e320851c3784cbb6f7741e7e7542adf9923bb0
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-09-12 22:43:18 -0300

    Entendendo o CRUD e metodos HTTP

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/usuarios", () =>
+{
+    List<string> nomes = new List<string>
+    {
+        "João",
+        "Maria",
+        "Luizinho",
+    };
+
+    return nomes;
+});
+
+app.MapPost("/usuarios", (string nome) =>
+{
+    return Results.Ok($"Usuário {nome} criado com sucesso!");
+});
+
+app.MapPut("/usuarios/{id}", (int id, string novoNome) =>
+{
+    return Results.Ok($"Usuário {id} atualizado para {novoNome}");
+});
+
+app.MapPatch("/usuarios/{id}", (int id, string campo, string valor) =>
+{
+    return Results.Ok($"Usuário {id} teve o campo {campo} atualizado para {valor}");
+});
+
+app.MapDelete("/usuarios/{id}", (int id) =>
+{
+    return Results.Ok($"Usuário {id} removido com sucesso!");
+});
 
 app.Run();
